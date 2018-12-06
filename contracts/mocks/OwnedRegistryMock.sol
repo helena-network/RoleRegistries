dcf17f25d5101246260dd086f56749aaaa0f992f
Joao Santos <jrmsantos15@gmail.com>
2018-12-06 10:02:29 +0000

    test(OwnedRegistryMock): improves coverage

--- contracts/mocks/OwnedRegistryMock.sol
+++ contracts/mocks/OwnedRegistryMock.sol
@@ -20,39 +20,37 @@ contract OwnedRegistryMock is OwnedRegistry {
         counter ++;
     }
 
-        // Debug
-
-    function debug_get0() public view returns (uint, address[5]){
-        return (strg[0].listingCounter, strg[0].whiteListed);
-    }
-    
-    function debug_get1() public view returns (uint, address[5]){
-        return (strg[1].listingCounter, strg[1].whiteListed);
-    }
-    
-    function debug_getCurrent() public view returns (uint, address[5]) {
-        if (height() > lastSync){
-            return (strg[1].listingCounter, strg[1].whiteListed);
-        }
-        return (strg[0].listingCounter, strg[0].whiteListed);
-    }
+    // Debug
 
     function debug_getArchive(uint256 _index) public view returns (address[5]){
         return _getEpochFromArchive(_index).whiteListed;
     }
 
-    function debug_getArchiveAndPeriod(uint256 _index) public view returns (uint256,address[5]){
-        Archive memory arch = archive[_index]; 
-        return (arch.epoch, arch.item.whiteListed);
-    }
-
-
-    function debug_getArchiveSize() public view returns (uint256){
-        return archive.length;
-    }
-
     function debug_forceUpdate() public updatable() {
         return;
     }
+    // function debug_get0() public view returns (uint, address[5]){
+    //     return (strg[0].listingCounter, strg[0].whiteListed);
+    // }
+    
+    // function debug_get1() public view returns (uint, address[5]){
+    //     return (strg[1].listingCounter, strg[1].whiteListed);
+    // }
+    
+    // function debug_getCurrent() public view returns (uint, address[5]) {
+    //     if (height() > lastSync){
+    //         return (strg[1].listingCounter, strg[1].whiteListed);
+    //     }
+    //     return (strg[0].listingCounter, strg[0].whiteListed);
+    // }
 
+    // function debug_getArchiveAndPeriod(uint256 _index) public view returns (uint256,address[5]){
+    //     Archive memory arch = archive[_index]; 
+    //     return (arch.epoch, arch.item.whiteListed);
+    // }
+
+
+    // function debug_getArchiveSize() public view returns (uint256){
+    //     return archive.length;
+    // }
 }
